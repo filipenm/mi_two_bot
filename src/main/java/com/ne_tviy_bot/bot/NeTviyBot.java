64a80bbb9ee7576b5a506e981686b154ee131297
filipenm <filipenm@fuib.com>
2022-08-16 00:14:19 +0200

    Reformat code

--- src/main/java/com/ne_tviy_bot/bot/NeTviyBot.java
+++ src/main/java/com/ne_tviy_bot/bot/NeTviyBot.java
@@ -7,7 +7,10 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 @Slf4j
@@ -25,9 +28,29 @@ public class NeTviyBot extends TelegramLongPollingBot {
         this.telegramFacade = telegramFacade;
     }
 
-
+    @Override
     public void onUpdateReceived(Update update) {
         telegramFacade.handleUpdate(update);
     }
 
+
+    public void sendMessage(long chatId, String textMessage) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(textMessage);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(SendMessage sendMessage) {
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
